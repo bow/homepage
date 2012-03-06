56ca3446b9cdb05f08ca81e549b6dc86efcacb9e
bow <bow@bow.web.id>
2012-03-06 08:00:51 +0100

    added: atom and disqus; updated: docs in voltconf

--- voltconf.py
+++ voltconf.py
@@ -1,92 +1,64 @@
 # Volt configurations file
 
+import os
+
 from volt.config.base import Config
 
 
 # Volt configurations
 VOLT = Config(
-
-    # Flag for colored terminal output
     COLORED_TEXT = True,
 )
 
-
 # General project configurations
 SITE = Config(
-
-    # Your site name
     TITLE = "bow.web.id",
-
-    # Your site URL
     URL = "http://bow.web.id",
-
-    # Your site description
     DESC = "Because static sites have potential",
-
-    # Engines used in generating the site
-    # Available engines are 'page', 'blog', and 'collection'
-    # To disable an engine, just remove its name from this list
     ENGINES = ['blog', 'plain'],
-
-    PLUGINS = (('volt-markdown', ('blog', 'plain')),),
+    PLUGINS = (('volt-markdown', ['blog', 'plain']),
+               ('volt-atomic', ['blog']),
+    ),
+    DISQUS_NAME = "bow",
 )
-
 
 # Blog engine configurations
 BLOG = Config(
-  
-    # URL for all blog content relative to root URL
     URL = "blog",
-
-    # Blog posts permalink, relative to blog URL
     PERMALINK = "{time:%Y/%m}/{slug}",
-
-    # Global values to be set to all blog posts
     GLOBAL_FIELDS = {'author': 'Wibowo Arindrarto', },
-
-    # Display datetime format
     DISPLAY_DATETIME_FORMAT = '%B %Y',
-
-    # The number of displayed posts per pagination page
     POSTS_PER_PAGE = 5, 
-
-    # Default length (in chars) of blog post excerpts
     EXCERPT_LENGTH = 400, 
-
-    # Packs
     PACKS = ('', 'category/{categories}', '{time:%Y/%m}', '{time:%Y}',),
 )
 
-
 # Page engine configurations
 PLAIN = Config(
-
-    # URL for all page content relative to root URL
     URL = "/",
-
-    # Page permalink, relative to page URL
     PERMALINK = "{slug}",
 )
 
+# Plugins configurations
+PLUGINS = Config(
+    ATOM_OUTPUT_FILE = os.path.join(os.getcwd(), 'site', 'atom.xml'),
+)
 
 # Jinja custom filters
 def appendhash(permalink, string):
-    """Append hash + string to the end of the given permalink.
-            
-    This is a Jinja2 filter used to append a given permalink with hash 
-    and the given string. All Volt permalinks ends with a slash, so
-    we need to use this filter to avoid placing the hash after the slash.
-    """
+    """Append hash + string to the end of the given permalink."""
     return permalink[:-1] + '#' + string
 
 def catlist(catlist):
+    """Show categories in comma-separated links."""
     s = []
     for cat in catlist:
         s.append('<a href="/blog/category/' + cat + '/" class="button red">' + cat+ '</a>')
     return ', '.join(s)
 
 def displaytime(time, format):
-    return time.strftime(format)
+    """Show time according to format."""
+    return time.strftime
 
 JINJA2 = Config(
     FILTERS = {
