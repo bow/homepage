6a278f1e74a3f2285bb278de0d94b13f72a2e8bf
bow <bow@bow.web.id>
2012-07-05 01:08:39 +0200

    Update intro page

--- templates/assets/js/custom.js
+++ templates/assets/js/custom.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
     $('a[href^="http"]')
         .attr('target', '_blank');
+
+    var caps = [
+        '<em>Vibrio cholerae</em> colonies on an agar plate',
+        'Gel shift assay of a cell extract',
+        'Mouse embryonic stem cells growing on fibroblasts',
+        'Proliferating <em>Macrostomum lignano</em> neoblasts',
+        'Donal, my cat :)'
+    ];
+
+    var counter = 0;
+    $('#intro figure img').click(function() {
+        var src = $(this).attr('src');
+        var idx = ++counter % caps.length;
+        $(this).attr('src', '/img/intro-0' + (idx + 1) + '.jpg');
+        $('#intro figure figcaption').html(caps[idx]);
+    });
 });
